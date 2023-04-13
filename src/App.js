e4fe2daba9b67ee83ed272a2b94ff1dc48986270
duc le <hoanggia92202@gmail.com>
2023-04-12 23:15:05 -0700

    update left bank

--- src/App.js
+++ src/App.js
@@ -2,41 +2,41 @@ import './App.css';
 
 const App = () => {
   return (
-    <div id='drum-machine' className='d-flex container-fluid'>
-      <div className='row bg-danger w-75 m-auto'>
-        <div className='col-sm-7'>
-          <div id='screen'></div>
-          <div className='mt-4 ml-auto mr-auto'>
-            <div id='q' className='drum-pad'>
-              <audio className='clip' id='Q'></audio>ooo
+    <div id='drum-machine' className='App d-flex container-fluid'>
+      <div className='container row w-75 m-auto'>
+        <div className='leftControl col-sm-7'>
+          <div id='screen' className='screen'></div>
+          <div className='keyRow mt-4 ml-auto mr-auto'>
+            <div id='q' className='keys drum-pad'>
+              <audio id='Q'></audio>Q
             </div>
-            <div id='w' className='drum-pad'>
-              <audio className='clip' id='W'></audio>W
+            <div id='w' className='keys drum-pad'>
+              <audio id='W'></audio>W
             </div>
-            <div id='e' className='drum-pad'>
-              <audio className='clip' id='E'></audio>E
-            </div>
-          </div>
-          <div className='mt-2 ml-auto mr-auto'>
-            <div id='a'>
-              <audio className='clip' id='A'></audio>A
-            </div>
-            <div id='s'>
-              <audio className='clip' id='S'></audio>S
-            </div>
-            <div id='d' className='drum-pad'>
-              <audio className='clip' id='D'></audio>D
+            <div id='e' className='keys drum-pad'>
+              <audio id='E'></audio>E
             </div>
           </div>
-          <div className='mt-2 ml-auto mr-auto'>
-            <div id='z'>
-              <audio className='clip' id='Z'></audio>Z
+          <div className='keyRow mt-2 ml-auto mr-auto'>
+            <div id='a' className='keys'>
+              <audio id='A'></audio>A
             </div>
-            <div id='x'>
-              <audio className='clip' id='X'></audio>X
+            <div id='s' className='keys'>
+              <audio id='S'></audio>S
             </div>
-            <div id='c'>
-              <audio className='clip' id='C'></audio>C
+            <div id='d' className='keys drum-pad'>
+              <audio id='D'></audio>D
+            </div>
+          </div>
+          <div className='keyRow mt-2 ml-auto mr-auto'>
+            <div id='z' className='keys'>
+              <audio id='Z'></audio>Z
+            </div>
+            <div id='x' className='keys'>
+              <audio id='X'></audio>X
+            </div>
+            <div id='c' className='keys'>
+              <audio id='C'></audio>C
             </div>
           </div>
         </div>
