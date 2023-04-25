d7be15bc7fa7c5a47ef64d2bc6d53e27a1511569
duc le <hoanggia92202@gmail.com>
2023-04-24 17:48:07 -0700

    remove all css classes

--- src/App.js
+++ src/App.js
@@ -2,57 +2,59 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className='container'>
-      <div className='padding'></div>
-      <div className='left-control'>
-        <div className='key-row'>
-          <div id='q' className='keys'>
-            <audio id='Q'></audio>Q
+    <div className='container-fluid bg-info'>
+      <div className='row'>
+        <div id='left-control' className='col-md-6'>
+          <div id='key-row' className='row'>
+            <div id='q' className='col-4'>
+              <audio id='Q'></audio>Q
+            </div>
+            <div id='w' className='col-4'>
+              <audio id='W'></audio>W
+            </div>
+            <div id='e' className='col-4'>
+              <audio id='E'></audio>E
+            </div>
           </div>
-          <div id='w' className='keys'>
-            <audio id='W'></audio>W
+          <div id='key-row' className='row'>
+            <div id='a' className='col-4'>
+              <audio id='A'></audio>A
+            </div>
+            <div id='s' className='col-4'>
+              <audio id='S'></audio>S
+            </div>
+            <div id='d' className='col-4'>
+              <audio id='D'></audio>D
+            </div>
           </div>
-          <div id='e' className='keys'>
-            <audio id='E'></audio>E
+          <div id='key-row' className='row'>
+            <div id='z' className='col-4'>
+              <audio id='Z'></audio>Z
+            </div>
+            <div id='x' className='col-4'>
+              <audio id='X'></audio>X
+            </div>
+            <div id='c' className='col-4'>
+              <audio id='C'></audio>C
+            </div>
           </div>
         </div>
-        <div className='key-row'>
-          <div id='a' className='keys'>
-            <audio id='A'></audio>A
+
+        <div id='right-control' className='col-md-6'>
+          <div id='switch-pad'>
+            <div id='power-container'></div>
+            <div id='display' className=''></div>
+            <div id='volume-container'>
+              <div id='volume-screen'></div>
+              <input id='volume-control' type='range' min='1' max='100' />
+            </div>
+            <div id='power-container'>
+              <button id='bank-button' className=''>
+                Bank
+              </button>
+            </div>
+            <div id='bank-name' className=''></div>
           </div>
-          <div id='s' className='keys'>
-            <audio id='S'></audio>S
-          </div>
-          <div id='d' className='keys'>
-            <audio id='D'></audio>D
-          </div>
-        </div>
-        <div className='key-row'>
-          <div id='z' className='keys'>
-            <audio id='Z'></audio>Z
-          </div>
-          <div id='x' className='keys'>
-            <audio id='X'></audio>X
-          </div>
-          <div id='c' className='keys'>
-            <audio id='C'></audio>C
-          </div>
-        </div>
-      </div>
-      <div className='right-control'>
-        <div className='switch-pad'>
-          <div className='power-container'></div>
-          <div id='display' className='lcd'></div>
-          <div className='volume-container'>
-            <div id='volume-screen' className='volume-screen'></div>
-            <input id='volume-control' className='slider-off' type='range' min='1' max='100' />
-          </div>
-          <div className='power-container'>
-            <button id='bank-button' className='bank-button'>
-              Bank
-            </button>
-          </div>
-          <div id='bank-name' className='bank-name'></div>
         </div>
       </div>
     </div>
