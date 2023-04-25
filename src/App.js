6b9f50c5d44d45df448418fbb6dee3beff665fa5
duc le <hoanggia92202@gmail.com>
2023-04-24 21:47:06 -0700

    add margin and padding to left-control

--- src/App.js
+++ src/App.js
@@ -3,44 +3,44 @@ import './App.css';
 const App = () => {
   return (
     <div className='App container-fluid bg-secondary' style={{ paddingTop: '7rem' }}>
-      <div className='row d-flex justify-content-center' style={{ height: '300px' }}>
+      <div className='row d-flex justify-content-center' style={{ minHeight: '300px' }}>
         <div id='left-control' className='col-md-5 d-flex flex-column justify-content-around border rounded-white'>
-          <div id='key-row' className='row d-flex justify-content-around'>
-            <div id='q' className='col-3 h1 border rounded text-center border-white bg-info'>
+          <div id='key-row' className='row pt-2 d-flex justify-content-around'>
+            <div id='q' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='Q'></audio>Q
             </div>
-            <div id='w' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='w' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='W'></audio>W
             </div>
-            <div id='e' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='e' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='E'></audio>E
             </div>
           </div>
           <div id='key-row' className='row d-flex justify-content-around'>
-            <div id='a' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='a' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='A'></audio>A
             </div>
-            <div id='s' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='s' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='S'></audio>S
             </div>
-            <div id='d' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='d' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='D'></audio>D
             </div>
           </div>
           <div id='key-row' className='row d-flex justify-content-around'>
-            <div id='z' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='z' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='Z'></audio>Z
             </div>
-            <div id='x' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='x' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='X'></audio>X
             </div>
-            <div id='c' className='col-3 h1 border rounded text-center border-white bg-info'>
+            <div id='c' className='col-3 h1 p-3 border rounded text-center border-white bg-info'>
               <audio id='C'></audio>C
             </div>
           </div>
         </div>
 
-        <div id='right-control' className='col-md-3 d-flex justify-content-center border rounded-white'>
+        <div id='right-control' className='col-md-3 d-flex justify-content-center border rounded-white bg-warning'>
           <div id='switch-pad' className='w-75 d-flex flex-column align-items-center'>
             <button type='button' className='w-50 btn btn-sm border rounded btn-danger' style={{ fontWeight: 'bolder' }}>
               ON/OFF
